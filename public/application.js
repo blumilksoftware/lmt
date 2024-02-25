cc416a63abaa802665d26e6ebb3dd65086ed6345
krzysztofrewak <krzysztof.rewak@gmail.com>
2024-02-25 16:31:50 +0100

    #4 - moving js away

---
+++ public/application.js
@@ -0,0 +1,51 @@
+function initialize() {
+    return {
+        meetups: [],
+        meetup: null,
+        selectedSpeaker: null,
+        counters: {
+            days: "00",
+            hours: "00",
+            minutes: "00",
+            seconds: "00",
+        },
+        start: async function () {
+            await this.fetchData()
+            this.calculateCounters()
+            setInterval(() => {
+                this.calculateCounters()
+            }, 1000)
+        },
+        fetchData: async function () {
+            const meetupsResponse = await fetch("./assets/meetups.json?timestamp=" + Date.now())
+            const meetupsData = await meetupsResponse.json()
+            this.meetups = meetupsData
+
+            const meetupResponse = await fetch(meetupsData[0] + "?timestamp=" + Date.now())
+            const meetupData = await meetupResponse.json()
+
+            let time = moment(meetupData.datetime).locale("pl")
+            meetupData["date"] = time.format("DD MMMM")
+            meetupData["time"] = time.format("HH:mm")
+            meetupData["year"] = time.format("Y")
+            meetupData["agenda"] = meetupData["agenda"].map(lecture => {
+                lecture["expanded"] = false
+                return lecture
+            })
+
+            this.selectedSpeaker = meetupData.lineup[0] ?? null
+            this.meetup = meetupData
+        },
+        calculateCounters: function () {
+            if (this.meetup) {
+                const now = moment()
+                const diff = moment(this.meetup.datetime).diff(now)
+
+                this.counters.days = String(Math.floor(moment.duration(diff).asDays())).padStart(2, "0")
+                this.counters.hours = String(moment.duration(diff).hours()).padStart(2, "0")
+                this.counters.minutes = String(moment.duration(diff).minutes()).padStart(2, "0")
+                this.counters.seconds = String(moment.duration(diff).seconds()).padStart(2, "0")
+            }
+        },
+    }
+}
