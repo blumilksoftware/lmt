a288a3594c17a970d2873ecce58989543e1ab724
Blusia <agnieszka.rudek@blumilk.pl>
2024-03-14 14:04:36 +0100

    - npx script

--- environment/prod/deployment/scripts/post-deploy-actions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# -e is for "automatic error detection", tell shell to abort any time an error occurred
-set -e
-
-composer install && \
-npm install && \
-npx tailwindcss -i ./styles.css -o ./public/output.css
