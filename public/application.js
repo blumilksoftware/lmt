8d4e9b3ee37a723d17f60707674d2dbcb75401e1
EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>
2024-09-20 10:49:37 +0200

    - wip

--- public/application.js
+++ public/application.js
@@ -62,6 +62,7 @@ function initialize() {
             meetupFolder
           )
 
+          meetupData.regulationsLink = `${meetupFolder}/${meetupData.regulations}`
           return meetupData
         })
       )
