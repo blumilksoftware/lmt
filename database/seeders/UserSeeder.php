147e17375d6ea6f579603ce44d5e6d5706b76c40
Blusia <agnieszka.rudek@blumilk.pl>
2025-04-18 10:10:25 +0200

    - add UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Blumilksoftware\Lmt\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            "name" => "Test User",
+            "email" => "admin@example.com",
+        ]);
+    }
+}
